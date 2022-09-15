373336ba2b591f847c4c0329dc47b4fd65954f0e
pooja8771 <pooja.singh8771@gmail.com>
2022-09-15 10:19:41 +0530

    typecasting

---
+++ src/Basics/TypeCasting.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        double myDouble= 9.76d;
+        int myInt= (int)myDouble;
+        System.out.println(myInt);
+        System.out.println(myDouble);
+    }
+}
