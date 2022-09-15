c7f030eb204c8ed8918c0828e1878cd1b5370bd9
pooja8771 <pooja.singh8771@gmail.com>
2022-09-15 10:33:16 +0530

    Initial Projects

---
+++ src/Basics/SumOfNumbers.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        int sum=1;
+        for(int i=1;i<=10;i++){
+            sum= sum+i;
+        }
+        System.out.println("Print numbers "+sum);
+    }
+}
